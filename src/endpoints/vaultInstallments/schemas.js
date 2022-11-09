1b7f5115da29dfec4b758c6a6f7737dba8939d30
Michel Abdala Salomon <abdalamichel@gmail.com>
2022-11-09 12:28:42 -0300

    campos installment

--- src/endpoints/vaultInstallments/schemas.js
+++ src/endpoints/vaultInstallments/schemas.js
@@ -8,10 +8,12 @@ const baseSchema = Joi.object({
   installmentNumber: Joi.number().allow(null),
   dueDate: Joi.date().allow(null),
   paymentStatus: Joi.string().allow(null, ''),
-  amount: Joi.number().allow(null),
-  amortizationAmount: Joi.number().allow(null),
-  nominalInterest: Joi.number().allow(null),
-  seals: Joi.number().allow(null),
+
+  debtAmount: Joi.number().allow(null),
+  principalAmount: Joi.number().allow(null),
+  interestAmount: Joi.number().allow(null),
+  taxesAmount: Joi.number().allow(null),
+  otherCosts: Joi.number().allow(null),
 
   notes: Joi.string().allow(''),
   attachments: Joi.any(),
